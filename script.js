adbb4dee97a32d38d429691894c89168b17e604e
Arunavi18 <115450437+Arunavi18@users.noreply.github.com>
2023-01-14 10:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let count;
+document.getElementById("evaluatedText");
+value="";
+document.addEventListener("keyup",function() {
+	count = document.getElementbyId("evaluatedText").value;
+	document.querySelector("#letterCount").textContent = count.length;
+})
